a54706449fab70e405283881304deaefa8b91b2c
Puppuccino <97849040+CrazyDubya@users.noreply.github.com>
2025-10-07 07:55:42 -0400

    Update Linear module __init__ with formatters and complete exports

--- src/research_scrapers/linear/__init__.py
+++ src/research_scrapers/linear/__init__.py
@@ -7,11 +7,48 @@ enabling automatic task creation and management from scraper results.
 Author: Stephen Thompson
 """
 
-from .client import LinearClient, LinearError, LinearRateLimitError, LinearAuthError
+from .client import (
+    LinearClient,
+    LinearError,
+    LinearRateLimitError,
+    LinearAuthError,
+    LinearValidationError,
+    LinearTeam,
+    LinearWorkflowState,
+    LinearIssue,
+    IssueState,
+    IssuePriority,
+    ScraperType,
+    create_linear_client,
+    create_issue_from_artifacts
+)
+
+from .formatters import LinearResultFormatter
 
 __all__ = [
+    # Client
     'LinearClient',
+    'create_linear_client',
+    'create_issue_from_artifacts',
+    
+    # Exceptions
     'LinearError',
     'LinearRateLimitError',
     'LinearAuthError',
+    'LinearValidationError',
+    
+    # Data Classes
+    'LinearTeam',
+    'LinearWorkflowState',
+    'LinearIssue',
+    
+    # Enums
+    'IssueState',
+    'IssuePriority',
+    'ScraperType',
+    
+    # Formatters
+    'LinearResultFormatter',
 ]
+
+__version__ = '1.0.0'
